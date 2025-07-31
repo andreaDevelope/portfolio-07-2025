eef4bff0b3b9a11bcd85020bb9cc401a5183a54f
andreaDevelope <andrea_develope@outlook.it>
2025-07-31 18:58:12 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './main-components/nav/nav.component';
+import { FooterComponent } from './main-components/footer/footer.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
