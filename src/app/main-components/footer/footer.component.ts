bb8526303bec6b9d8debb6105df4a489523b29d6
AndreaDevelope <andrea_develope@outlook.it>
2025-09-25 00:00:08 +0200

    Fix download CV button in footer component

--- src/app/main-components/footer/footer.component.ts
+++ src/app/main-components/footer/footer.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'assets/files/Andrea_Ceccarelli_CV.pdf';
+    link.download = 'Andrea_Ceccarelli_CV.pdf';
+    link.click();
+  }
 }
