8e75eb0e89539f43c6b5aeede683f6ec12bcdeb0
AndreaDevelope <andrea_develope@outlook.it>
2026-09-13 21:25:41 +0200

    fix: scroll to top on route navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
+import { ScrollToTop } from "./components/ScrollToTop";
 import { Home } from "./pages/Home";
 import { Projects } from "./pages/Projects";
 import { About } from "./pages/About";
@@ -10,6 +11,7 @@ import { Toaster } from "./components/ui/sonner";
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
